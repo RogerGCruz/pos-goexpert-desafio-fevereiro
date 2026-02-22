fee8420f31ccfe8ba7a79eaf164050ae887bfceb
Roger Cruz <92161038+RogerGCruz@users.noreply.github.com>
2026-02-22 19:04:01 -0300

    add client for request api and TODO list of full cicle scope

---
+++ src/client/client.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const (
+	urlCotationApi = "http://localhost:8080/cotacao"
+	pathFile       = "./cotation.txt"
+)
+
+type Cotation struct {
+	Bid string `json:"bid"`
+}
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", urlCotationApi, nil)
+	if err != nil {
+		log.Fatal("Error creating request:", err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal("Error making request (timeout?):", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Error reading response body: ", err)
+	}
+
+	var cotation Cotation
+	err = json.Unmarshal(body, &cotation)
+	if err != nil {
+		log.Fatal("Error parsing response JSON body:", err)
+	}
+
+	file, err := os.Create(pathFile)
+	if err != nil {
+		log.Fatal("Error creating file:", err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(fmt.Sprintf("Dólar: %s", cotation.Bid))
+	if err != nil {
+		log.Fatal("Error writing to file:", err)
+	}
+	log.Println("Cotation as saved in file:", file.Name())
+}
